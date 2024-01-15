b8ada9d5261d7ec02ed5ff29ab4bddc6137a33e7
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-01-15 22:19:41 +0530

    nav modified

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,9 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Navigation = () => {
 
     const { user, isAuthenticated, logout , loginWithRedirect} = useAuth0();
+    const navigate = useNavigate();
     
     const handleLogIn = ()=>{
         loginWithRedirect()
@@ -12,13 +13,17 @@ const Navigation = () => {
         logout()
     }
 
+    const navigateTo = (path) => () => {
+        navigate(path);
+    }
+
 
     return (
         <header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white text-sm py-3 md:py-0 ">
             <nav class="max-w-[85rem] w-full mx-auto px-4 md:px-6 lg:px-8" aria-label="Global">
                 <div class="relative md:flex md:items-center md:justify-between">
                     <div class="flex items-center justify-between">
-                        <a class="flex-none text-xl font-semibold" href="#" aria-label="Brand">{isAuthenticated? user.name: "Please Login"}</a>
+                        <button class="flex-none text-xl font-semibold" onClick={navigateTo("/")} aria-label="Brand">{isAuthenticated? user.name: "Please Login"}</button>
                         <div class="md:hidden">
                             <button type="button" class="hs-collapse-toggle flex justify-center items-center w-9 h-9 text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none " data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
                                 <svg class="hs-collapse-open:hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
@@ -30,7 +35,7 @@ const Navigation = () => {
                     <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block">
                         <div class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 ">
                             <div class="flex flex-col gap-x-0 mt-5 divide-y divide-dashed divide-gray-200 md:flex-row md:items-center md:justify-end md:gap-x-7 md:mt-0 md:ps-7 md:divide-y-0 md:divide-solid ">
-                                <a class="font-medium text-blue-600 py-3 md:py-6 " href="#" aria-current="page">Profile</a>
+                                <a class="font-medium text-blue-600 py-3 md:py-6 " onClick={navigateTo("/profile")} aria-current="page">Profile</a>
 
                                 <a class="font-medium text-gray-500 hover:text-gray-400 py-3 md:py-6 " href="#">
                                     Company
